12254f5339d5dadeaceb2b38612a04a3183ef2d9
Minh212 <minhhqgch190485@fpt.edu.vn>
2021-08-15 21:50:53 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+const {insertProduct,deleteProduct, getDB} = require('./databaseHandler');
+
+app.set('view engine','hbs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/add',(req,res)=>{
+    res.render("add");
+})
+
+app.post('/insert',async (req,res)=>{
+    const nameInput = req.body.txtName;
+    const priceInput = req.body.txtPrice;
+    const amountInput = req.body.txtAmount;
+    const newProduct = {name:nameInput,price:priceInput,amount:amountInput}
+
+    insertProduct(newProduct);
+    res.redirect("/");
+})
+app.get('/delete',async (req,res)=>{
+    const id = req.query.id;
+    
+    await deleteProduct(id);
+    res.redirect("/");
+})
+
+app.post('/search',async (req,res)=>{
+    const searchInput = req.body.txtSearch;
+    const dbo = await getDB()
+    const allProducts = await dbo.collection("products").find({name:searchInput}).toArray();
+
+    res.render('index',{data:allProducts})
+})
+
+app.get('/',async (req,res)=>{
+    const dbo = await getDB();
+    const allProducts = await dbo.collection("products").find({}).toArray();
+    res.render('index',{data:allProducts})
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
+console.log("app is running ",PORT)
---
+++ databaseHandler.js
@@ -0,0 +1,19 @@
+const {ObjectId,MongoClient} = require('mongodb');
+const url = 'mongodb+srv://hominh:minh212212@cluster0.xmrhr.mongodb.net/test';
+
+async function getDB() {
+    const client = await MongoClient.connect(url);
+    const dbo = client.db("StoreDB");
+    return dbo;
+}
+
+async function insertProduct(newProduct) {
+    const dbo = await getDB();
+    await dbo.collection("products").insertOne(newProduct);
+}
+
+async function deleteProduct(id) {
+    const dbo = await getDB();
+    await dbo.collection("products").deleteOne({ "_id": ObjectId(id) });
+}
+module.exports = {getDB,insertProduct,deleteProduct}
